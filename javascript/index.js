bffca8f6da86c0c60225aad18521d524d3d70af5
AmandaTodero <amanda.todero032@gmail.com>
2023-11-13 18:20:54 -0500

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -27,14 +27,15 @@ function updateCity(event) {
     let cityName = cityTimeZone.replace("_", "").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
-        <div class="city">
-            <div>
-                <h2>${cityName}</h2>
-                <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
-            </div>
-            <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
-        </div>`;
+ citiesElement.innerHTML = `
+    <div class="city">
+        <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>
+    <a href="index.html">All Cities</a>`;
 }
 
 
